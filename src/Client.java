6f8f9a67f2c777527cd0b656a828da719d10af3a
pierZz <piergm@usi.ch>
2014-03-26 19:35:27 +0100

    Added Client Class

---
+++ src/Client.java
@@ -0,0 +1,88 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+/**
+ * Created by matteo piergiovanni on 24.03.14.
+ */
+
+public class Client {
+    public static void main(String[]args) throws IOException {
+        Client x=new Client("10.62.162.205", 4444);
+    }
+    public SocketChannel sc;
+
+    public Client(String ip, int porta) throws IOException {
+
+        sc = SocketChannel.open();
+        sc.connect(new InetSocketAddress(ip, porta));
+        Recive r;
+        r = new Recive(sc);
+        Thread a = new Thread(r);
+        a.start();
+        Send q;
+        q = new Send(sc);
+        Thread b = new Thread(q);
+        b.start();
+    }
+
+}
+
+class Recive extends Thread{
+    SocketChannel sc;
+    public Recive(SocketChannel sc) {
+        this.sc = sc;
+    }
+    public void run(){
+        while(true) {
+            try {
+                ByteBuffer buff = ByteBuffer.allocate(512);
+                buff.clear();
+                int nbyte = sc.read(buff);
+                String str = "";
+                if (nbyte != -1) {
+                    buff.flip();
+                    while (buff.hasRemaining()) {
+                        str += (char) buff.get();
+                    }
+                    System.out.println(str);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+}
+
+
+class Send extends Thread{
+    SocketChannel sc;
+    public Send(SocketChannel sc) {
+        this.sc = sc;
+    }
+    public void run() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String msg;
+            msg = scanner.nextLine();
+            try {
+                ByteBuffer buff = ByteBuffer.allocate(512);
+                buff.clear();
+                buff.put(msg.getBytes());
+                buff.flip();
+                sc.write(buff);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+    }
+
+}
