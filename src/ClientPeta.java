d2d15c7aee5a3f69d1eefbb7db6502f2fef752ee
pierZz <piergm@usi.ch>
2014-05-02 17:39:25 +0200

    Aggiunto la possibilità di gestire FPS
    Manda 10 “send” al secondo

--- src/ClientPeta.java
+++ src/ClientPeta.java
@@ -12,28 +12,20 @@ public class ClientPeta {
         ClientPeta x=new ClientPeta("10.62.162.84", 4444);
     }
     public SocketChannel sc;
+    public int randomNum;
     public ClientPeta(String ip, int porta) throws IOException {
-
+        randomNum = (int)(Math.random()*1000);
+        String msg = "BENFAGAYY";
         sc = SocketChannel.open();
         sc.connect(new InetSocketAddress(ip, porta));
         Receive2 r;
         r = new Receive2(sc);
         Thread a = new Thread(r);
         a.start();
-        sender("BENFAGAYYYYYY");
-    }
-
-    public void sender(String msg) {
-        try {
-            ByteBuffer buff = ByteBuffer.allocate(512);
-            buff.clear();
-            buff.put(msg.getBytes());
-            buff.flip();
-            sc.write(buff);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
+        Send2 q;
+        q = new Send2(sc, msg, randomNum);
+        Thread b = new Thread(q);
+        b.start();
     }
 
 }
@@ -45,6 +37,7 @@ class Receive2 extends Thread{
     }
     public void run(){
         while(true) {
+
             try {
                 ByteBuffer buff = ByteBuffer.allocate(512);
                 buff.clear();
@@ -60,10 +53,49 @@ class Receive2 extends Thread{
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
 
     }
 
 
+
+}
+
+
+class Send2 extends Thread{
+    private SocketChannel sc;
+    private String msg;
+
+    public Send2(SocketChannel sc, String msg, int id) {
+        this.sc = sc;
+        this.msg = "ID: "+id+"   ->"+msg;
+
+    }
+    public void run() {
+        long taskTime = 0;
+        long sleepTime = 1000/10;
+        while (true) {
+            taskTime = System.currentTimeMillis();
+            try {
+                ByteBuffer buff = ByteBuffer.allocate(512);
+                buff.clear();
+                buff.put(msg.getBytes());
+                buff.flip();
+                sc.write(buff);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            taskTime = System.currentTimeMillis()-taskTime;
+            if (sleepTime-taskTime > 0 ) {
+                try {
+                    Thread.sleep(sleepTime-taskTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
 
 }
